29d7cc6a9b2f9e3a6a6ab22157cfc213ad18cb18
Lauritsen1 <43439056+Lauritsen1@users.noreply.github.com>
2020-06-29 21:11:13 +0200

    Header åbntingstider
    Tilføjet kasse i header med åbnings status og tilsluttet database så der står om der er åbent eller lukket efter boolean på firebase

---
+++ src/js/script.js
@@ -0,0 +1,31 @@
+// if (time.getHours() <= 10) {
+//     console.log('Vi har åbent')
+// }
+
+// db.collection("openingHours")
+//     .doc('1YEFNC6gZtw4z17UBxDn')
+//     .update({
+
+//     });
+
+db.collection("openingHours")
+    .get()
+    .then(function (querySnapshot) {
+        querySnapshot.forEach(function (doc) {
+            let data = doc.data();
+
+            let openingHours = document.querySelector('.open__message');
+            let indicator = document.querySelector('.open__indicator');
+
+            if (data.open == true) {
+                openingHours.textContent = 'Åbent nu, vi lukker kl 22:00';
+                indicator.style.backgroundColor = 'lightgreen';
+            } else {
+                openingHours.textContent = 'Lukket, vi åbner kl 10:00';
+                indicator.style.backgroundColor = 'red';
+            }
+        });
+    })
+    .catch(function (error) {
+        console.log("Error getting documents: ", error);
+    });
