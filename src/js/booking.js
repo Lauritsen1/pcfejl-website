6343331c2035eed288536557838d5a0b4a2d11df
Lauritsen1 <43439056+Lauritsen1@users.noreply.github.com>
2020-07-25 21:52:25 +0200

    maker over på pris siden og laver service side

--- src/js/booking.js
+++ src/js/booking.js
@@ -1,22 +1,5 @@
 let timePoints = document.querySelectorAll('#time option');
 
-db.collection("booking")
-    .get()
-    .then(function (querySnapshot) {
-        querySnapshot.forEach(function (doc) {
-            let data = doc.data();
-
-            timePoints.forEach(point => {
-                if (point.value == data.time) {
-                    point.remove();
-                }
-            });
-        });
-    })
-    .catch(function (error) {
-        console.log("Error getting documents: ", error);
-    });
-
 let form = document.querySelector('.booking-form');
 const name = document.querySelector('#name');
 const email = document.querySelector('#email');
@@ -53,15 +36,4 @@ form.addEventListener('submit', (event) => {
         return false;
     }
 
-    db.collection("booking")
-        .add({
-            name: form.name.value,
-            email: form.email.value,
-            tlf: form.tlf.value,
-            day: form.day.value,
-            time: form.time.value,
-            message: form.message.value,
-        }).then(function () {
-            window.location.replace(window.location);
-        })
 });
