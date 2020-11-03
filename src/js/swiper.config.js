13049f178110443ae97e4e060435739acd42815a
Lauritsen1 <43439056+Lauritsen1@users.noreply.github.com>
2020-11-03 21:38:42 +0100

    Nyt design til forsiden

---
+++ src/js/swiper.config.js
@@ -0,0 +1,38 @@
+var mySwiper = new Swiper('.swiper-container', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+    // spaceBetween: 8,
+    
+    slidesPerView: 3,
+
+    breakpoints: {
+        320: {
+            slidesPerView: 1,
+        },
+        // when window width is <= 499px
+        768: {
+            slidesPerView: 2,
+        },
+        // when window width is <= 999px
+        1100: {
+            slidesPerView: 3,
+        },
+    },
+
+        // If we need pagination
+        pagination: {
+        el: '.swiper-pagination',
+    },
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+    // And if we need scrollbar
+    scrollbar: {
+        el: '.swiper-scrollbar',
+    },
+})
